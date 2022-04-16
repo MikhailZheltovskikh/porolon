59039711aa31a4272104c861cd940155b9e264a0
Mikhail_Zheltovskikh <misha14051994@mail.ru>
2022-04-16 12:10:54 +0500

    added section

---
+++ app/js/main.js
@@ -0,0 +1,58 @@
+$('.certificates__slider').slick({
+   dots: false,
+   slidesToShow: 3,
+   slidesToScroll: 1,
+   arrows: true,
+   nextArrow: '<button class="slick-next slick-next"><img src="images/icon/slide-next.svg" alt="next arrow"></button>',
+   prevArrow: '<button class="slick-prev slick-arrow"><img src="images/icon/slide-prev.svg" alt="next arrow"></button>',
+   // responsive: [
+   //    {
+   //       breakpoint: 1251,
+   //       settings: {
+   //          slidesToShow: 3
+   //       },
+   //    },
+   //    {
+   //       breakpoint: 901,
+   //       settings: {
+   //          slidesToShow: 2
+   //       }
+   //    },
+   //    {
+   //       breakpoint: 731,
+   //       settings: {
+   //          slidesToShow: 1
+   //       },
+   //    },
+   // ]
+});
+
+
+$('.popular-products__slider').slick({
+   dots: false,
+   slidesToShow: 4,
+   slidesToScroll: 1,
+   arrows: true,
+   nextArrow: '<button class="slick-next slick-next"><img src="images/icon/slide-next.svg" alt="next arrow"></button>',
+   prevArrow: '<button class="slick-prev slick-arrow"><img src="images/icon/slide-prev.svg" alt="next arrow"></button>',
+   // responsive: [
+   //    {
+   //       breakpoint: 1251,
+   //       settings: {
+   //          slidesToShow: 3
+   //       },
+   //    },
+   //    {
+   //       breakpoint: 901,
+   //       settings: {
+   //          slidesToShow: 2
+   //       }
+   //    },
+   //    {
+   //       breakpoint: 731,
+   //       settings: {
+   //          slidesToShow: 1
+   //       },
+   //    },
+   // ]
+});
